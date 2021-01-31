e23537a32003e15df8de1379d64a7e6fb9e6ec88
Jordi Santos <jordiws@gmail.com>
2021-01-31 20:16:31 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/jws/workshopmongo/domain/User.java
+++ src/main/java/com/jws/workshopmongo/domain/User.java
@@ -1,12 +1,16 @@
 package com.jws.workshopmongo.domain;
 
-import java.io.Serializable;
-import java.util.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
+@Document(collection = "user")
 public class User implements Serializable {
 
     public static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/jws/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jws.workshopmongo.repository;
+
+import com.jws.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+
+}
