dfa7e237950be3db0623243f2c7a207131c79095
Jordi Santos <jordiws@gmail.com>
2021-02-01 19:22:12 -0300

    Delete is finally working

--- src/main/java/com/jws/workshopmongo/services/UserService.java
+++ src/main/java/com/jws/workshopmongo/services/UserService.java
@@ -17,7 +17,7 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
 
@@ -26,16 +26,27 @@ public class UserService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public User insert(User user){
-        return repo.insert(user);
-    }
-
-    public User fromDTO(UserDTO objDto){
-        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    public User insert(User obj) {
+        return repo.insert(obj);
     }
 
     public void delete(String id) {
         findById(id);
         repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
