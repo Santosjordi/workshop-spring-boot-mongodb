a998f20e5a6b10c98de6a85e3d930f30aeeaf8ce
Jordi Santos <jordiws@gmail.com>
2021-02-01 20:11:40 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/jws/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jws/workshopmongo/config/Instantiation.java
@@ -1,14 +1,18 @@
 package com.jws.workshopmongo.config;
 
+import com.jws.workshopmongo.domain.Post;
 import com.jws.workshopmongo.domain.User;
+import com.jws.workshopmongo.repository.PostRepository;
 import com.jws.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -16,17 +20,27 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        userRepository.save(maria);
-        userRepository.save(alex);
-        userRepository.save(bob);
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/jws/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jws/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,8 @@
 package com.jws.workshopmongo.repository;
 
-import com.jws.workshopmongo.domain.User;
+import com.jws.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> { }
+public interface PostRepository extends MongoRepository<Post, String> { }
